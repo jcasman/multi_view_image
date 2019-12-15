47076cacefe7fd4b3d8383d330ba71dc9cf2bb5a
NaokiSato102 <s17C1066NQ@s.chibakoudai.jp>
2019-12-15 18:08:30 +0900

    rename&add sleep to main.py

--- main.py
+++ main.py
@@ -3,10 +3,14 @@
 
 # pylint: disable=C0111
 # ↑プログラムの説明ドキュメントがないよ！というエラーの防止
+import time
 import PyTheta
 
 t_list = PyTheta.connect_init()
 print("実行結果{},type={}".format( t_list,type(t_list) ) )
 
 PyTheta.start_capture(t_list)
+
+time.sleep(10)
+
 PyTheta.finish_capture(t_list)
