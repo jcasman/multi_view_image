76e9fe44d7343bcf9a0c26a10f7ea4691bd5e70b
NaokiSato102 <s17C1066NQ@s.chibakoudai.jp>
2020-01-24 22:42:35 +0900

    commit

--- scripts/bat_rem_time_pub.py
+++ scripts/bat_rem_time_pub.py
@@ -14,32 +14,9 @@ from std_msgs.msg import Int32MultiArray
 
 
 
-flag = False
-data_X = np.empty( (0,2),int )
-data_Y = np.empty( (0,3),int )
 
 def callback(message):
-	global flag
-	global data_X
-	global data_Y
-	data_Ans = []
-
-	nowtime = time.time()
-
-	data_X = np.vstack( [data_X, np.array([nowtime,1] ) ] )
-	data_Y = np.vstack( [data_Y, np.array(message.data) ] )
-	
-	#rospy.loginfo("get message! (\n%s)", data_Y)
-	
-	for n in range(data_Y.shape[1] ):
-		
-		a,b = np.linalg.lstsq(data_X,data_Y[:,n])[0]
-		
-		data_Ans.append( b/a - nowtime )
-
-	rospy.loginfo("get message! (%s)", data_Ans)
-
-	
+	rospy.loginfo("Get battery Lv! (%s)", message.data)
 
 if(__name__ == '__main__'):
 	rospy.init_node('bat_rem_time_pub')
