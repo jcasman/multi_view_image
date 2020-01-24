91df48a00db0c1c4fdd8aafb8a800284629a82c6
NaokiSato102 <s17C1066NQ@s.chibakoudai.jp>
2020-01-24 13:20:11 +0900

    rename & small change

--- scripts/bat_lv_pub.py
+++ scripts/bat_lv_pub.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import rospy
-import pytheta
+# pylint: disable=C0111
+# ↑プログラムの説明ドキュメントがないよ！というエラーの防止
 
+import rospy
 from std_msgs.msg import Int32MultiArray
+
+import pytheta
 
 rospy.init_node('bat')
 pub = rospy.Publisher('bat_lv', Int32MultiArray, queue_size=1)
-rate = rospy.Rate(0.05)
+rate = rospy.Rate(0.5)
 
 t_list = pytheta.connect_init()
 print("実行結果{},type={}".format( t_list,type(t_list) ) )
 
-
-
 while not rospy.is_shutdown():
-    pub.publish(
+	pub.publish(
 		Int32MultiArray(data = pytheta.get_bat_lv(t_list) ) 
 	)
-    rate.sleep()
+	rate.sleep()
 
 print("\nend")
